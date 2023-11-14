236c6877a876abae0cb61fed7c2e45d075f76289
David Falla Urtiaga <falla.urdav24@triana.salesianos.edu>
2023-11-14 13:55:10 +0100

    OOOOOHHH MY GOD

--- holaMundo.java
+++ holaMundo.java
@@ -3,6 +3,8 @@
 public class holaMundo {
 
     public static void main (String[]args){
+        System.out.println("*". repeat(10));
         System.out.println("Hola Mundo");
+        System.out.println("*". repeat(10));
     }
 }
